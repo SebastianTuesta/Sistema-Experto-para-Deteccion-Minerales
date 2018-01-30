2ff93bcc4346a77bad425f825a1039927c4de1e5
Sebastian <tuestagabo@gmail.com>
2018-01-29 22:49:36 -0500

    Proyecto versión Beta

---
+++ src/ia_se_deteccion_minerales/IA_SE_Deteccion_Minerales.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ia_se_deteccion_minerales;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class IA_SE_Deteccion_Minerales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Sistema_Experto SE = new Sistema_Experto();
+        SE.setVisible(true);
+        SE.setLocationRelativeTo(null);
+        SE.setTitle("SISTEMA EXPERTO");
+    }
+    
+}
